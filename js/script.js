a1733554c20bf57bc8d365638a068bcbea0de57f
Alex <alexvictar@mail.ru>
2021-06-27 22:24:13 +0600

    Header

--- js/script.js
+++ js/script.js
@@ -2,7 +2,12 @@ window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
 
-	var scene = document.getElementById('scene');
-    var parallax = new Parallax(scene);
+	var scene1 = document.getElementById('scene1');
+    var parallax1 = new Parallax(scene1);
 
+	var scene2 = document.getElementById('scene2');
+    var parallax2 = new Parallax(scene2);
+
+	var scene3 = document.getElementById('scene3');
+    var parallax3 = new Parallax(scene3);
 });
